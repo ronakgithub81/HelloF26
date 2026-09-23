989578fec1b8a99a2ee99a4a79f8d51a0228f5a0
Ronak Sheth <Ronak.Sheth@sheridancollege.ca>
2026-09-23 10:42:29 -0400

    added new class called part time student

---
+++ src/hellof26/FullTimeStudent.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hellof26;
+
+/**
+ *
+ * @author Ronak
+ */
+public class FullTimeStudent extends Student {
+
+    public FullTimeStudent(String sID, String sName) {
+        super(sID, sName);
+    }
+    
+}
